14b3f1f2ad065bca886190b9f3fa906d23eb1525
Miguel Lopez <hello@miguellopez.dev>
2021-09-11 13:33:19 -0700

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ const multiply = function (num1, num2) {
 const divide = function (num1, num2) {
 	return num1 / num2;
 };
+const operate = function (num1, op, num2) {
+	s = op;
+	switch (s) {
+		case 'x':
+			return multiply(num1, num2);
+
+		case 'd':
+			return divide(num1, num2);
+
+		default:
+			break;
+	}
+};
