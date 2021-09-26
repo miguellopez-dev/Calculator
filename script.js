ba6f48c5b547f62e636b890c0a72d67571163ac6
Miguel Lopez <hello@miguellopez.dev>
2021-09-26 06:11:27 -0700

    Change multiply position and edit switch statment with correct symbols

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let display = document.querySelector('.calc__screen');
+let bttnNum = document.querySelectorAll('.num');
+let op = document.querySelectorAll('.op');
+
 const add = function (num1, num2) {
 	return num1 + num2;
 };
@@ -13,14 +17,21 @@ const multiply = function (num1, num2) {
 const divide = function (num1, num2) {
 	return num1 / num2;
 };
+
 const operate = function (num1, op, num2) {
 	s = op;
 	switch (s) {
-		case 'x':
+		case 'X':
 			return multiply(num1, num2);
 
-		case 'd':
+		case '/':
 			return divide(num1, num2);
+
+		case '-':
+			return subtract(num1, num2);
+
+		case '+':
+			return add(num1, num2);
 
 		default:
 			break;
