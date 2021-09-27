fcd2d84c11392a2bbaecf5d984030483b6b78532
Miguel Lopez <hello@miguellopez.dev>
2021-09-27 08:10:15 -0700

    Working calculator, can do basic operations. Add all event listeners to buttons

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
 let display = document.querySelector('.calc__screen');
 let bttnNum = document.querySelectorAll('.num');
 let op = document.querySelectorAll('.op');
+let eq = document.querySelector('.eq');
+
+let num1 = 0;
+let num2 = 0;
+let opSymbol;
+let finalNumber;
+
+bttnNum.forEach((el) => {
+	el.addEventListener('click', () => {
+		if (num1 == 0) {
+			num1 = el.innerHTML;
+			display.innerHTML = num1;
+		} else if (finalNumber) {
+			finalNumber = undefined;
+			num1 = el.innerHTML;
+			display.innerHTML = num1;
+		} else {
+			num1 += el.innerHTML;
+			display.innerHTML = num1;
+		}
+	});
+});
+
+op.forEach((el) => {
+	el.addEventListener('click', () => {
+		opSymbol = el.innerHTML;
+		display.innerHTML = 0;
+		num2 = num1;
+		num1 = 0;
+	});
+});
+
+eq.addEventListener('click', () => {
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	console.log(typeof num1);
+	operate(num2, opSymbol, num1);
+	display.innerHTML = finalNumber;
+});
 
 const add = function (num1, num2) {
-	return num1 + num2;
+	return (finalNumber = num1 + num2);
 };
 
 const subtract = function (num1, num2) {
-	return num1 - num2;
+	return (finalNumber = num1 - num2);
 };
 
 const multiply = function (num1, num2) {
-	return num1 * num2;
+	return (finalNumber = num1 * num2);
 };
 
 const divide = function (num1, num2) {
-	return num1 / num2;
+	return (finalNumber = num1 / num2);
 };
 
 const operate = function (num1, op, num2) {
